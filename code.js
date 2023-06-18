7f7c2bbbf77c38855381de2d9d228edffd84886d
huygamcha <lehuynhhuy2002@gmail.com>
2023-06-18 17:21:34 +0700

    up

--- code.js
+++ code.js
@@ -2,14 +2,18 @@ const showBtn = document.querySelector('.user')
 const showOne = document.querySelector('.userone-show')
 const showTwo = document.querySelector('.usertwo-show')
 const changeBtns = document.querySelectorAll('.change-state')
+const modalBtn = document.querySelector('.modalhand')
 showBtn.addEventListener('click', function () {
     if (showOne.classList.contains('active') || showTwo.classList.contains('active')) {
         showTwo.classList.remove('active')
         showOne.classList.remove('active')
+        modalBtn.classList.remove('active')
+
     }
     else if (!showTwo.classList.contains('active') && !showOne.classList.contains('active')) {
         showOne.classList.add('active')
         showTwo.classList.remove('active')
+        modalBtn.classList.add('active')
     }
 })
 for (const changeBtn of changeBtns) {
@@ -25,3 +29,14 @@ for (const changeBtn of changeBtns) {
     })
 
 }
+modalBtn.addEventListener('click', function () {
+    modalBtn.classList.remove('active')
+    showOne.classList.remove('active')
+    showTwo.classList.remove('active')
+})
+showTwo.addEventListener('click', function (e) {
+    e.stopPropagation()
+})
+showOne.addEventListener('click', function (e) {
+    e.stopPropagation()
+})
